49faee80f42c04714f713510ecb15b2f8097d6b6
Lukas <xd3433660@gmail.com>
2026-09-10 21:03:54 -0300

    feat(core/logica): implementa a função de listar alunos do sistema.

--- core/logica.py
+++ core/logica.py
@@ -1,4 +1,16 @@
-# Módulo para a lógica da aplicação, funções do CRUD como: Adicionar alunos, Listar Alunos, Buscar aluno, Remover aluno, Editar aluno e também gera um relatório!
+# Módulo para a lógica da aplicação, funções do CRUD.
+
+"""
+Funções:
+1. Adicionar Aluno
+2. Listar Aluno
+3. Buscar Aluno
+4. Excluir Aluno
+5. Editar Aluno
+6. Gerar Relatório
+"""
+
+# Funções do CRUD
 
 def adicionar_aluno(nome, notas, descricao):
     if not nome.strip() or not descricao.strip():
@@ -26,3 +38,14 @@ def adicionar_aluno(nome, notas, descricao):
     }
 
     return aluno_dicionario
+
+def listar_alunos(sistema):
+    if not isinstance(sistema, list):
+        raise TypeError("Sistema deve ser uma lista.")
+
+    if not sistema:
+        raise TypeError("Sem alunos no sistema!")
+
+    for aluno in sistema:
+        situacao_do_aluno = "Aprovado" if aluno['situacao'] else "Reprovado"
+        print(f"- {aluno['nome'].capitalize()} | Média: {aluno['media']:.2f} | {situacao_do_aluno} | {aluno['informacao']}")
